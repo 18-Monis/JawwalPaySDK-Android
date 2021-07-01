7acbf63d8efded4d43dc93c451eb782196636a8c
your_username <mattili@bci.ps>
2021-07-01 14:14:33 +0300

    save logutil class

---
+++ app/src/main/java/com/bci/jawwalpaysdk/LogUtil.java
@@ -0,0 +1,9 @@
+package com.bci.jawwalpaysdk;
+
+import android.util.Log;
+
+public class LogUtil {
+    public void showLog(String message){
+        Log.d(LogUtil.class.toString() , message);
+    }
+}
